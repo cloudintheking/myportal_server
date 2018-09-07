edae147c7684dd540a766bf3821857c4874528e8
ahuang <2424897346@qq.com>
2018-09-07 16:35:00 +0800

    新增jasypt加密配置文件

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 - rabbitmq 3.7.3
 - mongodb 3.6
 - maven 3.5.4
->环境可以跟我的不一样，但一定得都有，否则你即便代码拉下来也无法运行。
+>环境可以跟我的不一样，但一定得都有，否则代码拉下来也运行不了。
 
 ##运行
 如果环境都安装无误的话
@@ -26,3 +26,7 @@
 >打开[http://http://localhost:8882/swagger-ui.html#](http://localhost:8882/swagger-ui.html#),查看文件系统API文档
 
 >rabbitmaq的消息打印在console里，暂时只写了用户注册队列。
+
+>另外项目使用jasypt加密了配置文件
+##后续
+    最近忙着刷题，以后有时间我会把shiro也整合进来
--- backsystem/src/main/java/co/fatboa/backsystem/BackSystemApplication.java
+++ backsystem/src/main/java/co/fatboa/backsystem/BackSystemApplication.java
@@ -1,11 +1,15 @@
 package co.fatboa.backsystem;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 /**
  * @Author: hl
